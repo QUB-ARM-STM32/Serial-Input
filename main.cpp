f2810dac8b8abac29011da5519e1eaadc2af291c
DogRacer78 <doggieracer@hotmail.com>
2023-07-04 12:52:20 +0000

    Completed Code Tutorial

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include "mbed.h"
 
+// define the buffered serial to use
+static BufferedSerial pc(USBTX, USBRX);
+
+// define the pin outs
+DigitalOut greenLED(LED1);
+
+// buffer for our character
+char* buffer = new char[1];
+
 // main() runs in its own thread in the OS
 int main()
 {
+
     while (true) {
+        pc.read(buffer, 1);
+
+
+        if (*buffer == '1'){
+            greenLED = 1;
+            printf("\r\nGREEN LED ON\r\n");
+        }
+        else if (*buffer == '0'){
+            greenLED = 0;
+            printf("\r\nGREEN LED OFF\r\n");
+        }
+        else{
+            printf("\r\nYou entered %c\r\n", *buffer);
+        }
+
 
     }
 }
